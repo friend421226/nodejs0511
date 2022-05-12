7bd0cbf425502dc7a37202d37a8b8e32eebba74e
friend421226 <friend421226@gmail.com>
2022-05-12 10:58:11 +0800

    3-4製作 login 頁面並處理 POST request（第三方套件 body-parser）

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // 第一個區塊 內建模組
 const path = require('path');
+
 // 第二個區塊 第三方模組(套件)
 const express = require('express');
+const bodyParser = require('body-parser');
+
 // 第三個區塊 自建模組
-////////////////////////////////////////////////////////////////
+
 const app = express();
+
 // middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
 	console.log('Hello!');
     next();
@@ -14,22 +21,25 @@ app.use((req, res, next) => {
 	console.log('World!');
     next();
 });
-
 app.get('/', (req, res) => {
-    // res.writeHead(200, { 'Content-Type': 'text/html' });
-    // res.write('<head><meta charset="utf-8" /></head>')
-    // res.write('<body>')
-    // res.write('<h1>這是首頁</h1>')
-    // res.write('</body>')
     res.status(200)
         .sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-
 app.get('/login', (req, res) => {
     res.status(200)
         .sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    console.log('form data: ', req.body);
+    if (email && password) {
+        res.redirect('/');
+    } else {
+        console.log('欄位尚未填寫完成！')
+    }
+});
+
 app.listen(3000, () => {
 	console.log('Web Server is running on port 3000');
-});
+}); 
