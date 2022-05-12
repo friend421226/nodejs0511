76035f91bfe364372d076b841b4911371128b03a
friend421226 <friend421226@gmail.com>
2022-05-12 09:22:14 +0800

    3-2b製作login page

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 // 第一個區塊 內建模組
 const path = require('path');
-
 // 第二個區塊 第三方模組(套件)
 const express = require('express');
-
 // 第三個區塊 自建模組
-
 ////////////////////////////////////////////////////////////////
-
 const app = express();
-
 // middleware
 app.use((req, res, next) => {
 	console.log('Hello!');
     next();
 });
-
 app.use((req, res, next) => {
 	console.log('World!');
     next();
@@ -29,6 +23,11 @@ app.get('/', (req, res) => {
     // res.write('</body>')
     res.status(200)
         .sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.status(200)
+        .sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
 app.listen(3000, () => {
