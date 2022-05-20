b96be741727a4e0ce7a16b8e884c963fcd705522
friend421226 <friend421226@gmail.com>
2022-05-20 14:13:30 +0800

    7-2 註冊功能(2) 實作 postSignup 控制

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,10 @@
+
+   
 const Sequelize = require('sequelize');
 
 ////////////////////////////////////////////////////////////
 
+// database 是 Sequelize class 的實例(實體, instance) 
 const database = new Sequelize('playground', 'root', '12345678', {
     dialect: 'mysql', 
     host: 'localhost'
